3bed785a98cb525dcd8dfedfee1032a88eee0d88
Poojitha1616 <gudemsaipoojitha2@gmail.com>
2025-01-28 22:31:20 +0530

    Add files via upload
    this program displays the swapping of two variables.

---
+++ Swap.java
@@ -0,0 +1,15 @@
+//•	Write a program to swap two variables using a third variable.
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("Before swapping: a = " + a + ", b = " +b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
